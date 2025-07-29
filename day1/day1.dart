c548cc68b4c607511731cce7e9f0b35dcb7e156b
usamah ibadurrohman <apalahajykan@gmail.com>
2025-07-29 07:36:03 +0700

    day1

---
+++ day1/day1.dart
@@ -0,0 +1,13 @@
+// temperature
+void main(){
+  int suhu =40;
+  if (suhu > 38){
+    print("panasss bosss");
+  
+  }else if (suhu < 15){
+    print("tiriss bossss");
+  }else {
+    print("aman boss");
+  }
+   
+}
